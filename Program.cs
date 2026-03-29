8f56412cd827f2db9a51e48b6e0b4e044250481b
Vincent Dizon <workvpdizon@gmail.com>
2026-03-30 07:47:46 +0800

    added serilog config

--- Program.cs
+++ Program.cs
@@ -1,23 +1,50 @@
 using poker_planning_api.Infrastructure;
 using Scalar.AspNetCore;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddOpenApi();
+    builder.Services.AddInfrastructure(builder.Configuration);
 
-var app = builder.Build();
+// serilog config.
+    builder.Host.UseSerilog((context, services, config) => config.ReadFrom.Configuration(context.Configuration).ReadFrom
+        .Services(services).Enrich.FromLogContext());
+
+    var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+        app.MapScalarApiReference();
+    }
+
+    app.UseSerilogRequestLogging(options =>
+    {
+        options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+        {
+            diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+            diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+            diagnosticContext.Set("TraceIdentifier", httpContext.TraceIdentifier);
+        };
+    });
+
+
+    app.UseHttpsRedirection();
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-
-app.Run();
+catch (Exception e)
+{
+    Log.Fatal(e, "Program terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
